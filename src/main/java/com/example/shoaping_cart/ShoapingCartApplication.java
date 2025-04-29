8f4e4cde30485ad75ac27b62e9a0dc90d1cb9045
Tanishq Dangi <td503134@gmail.com>
2025-04-30 05:25:13 +0530

    init commit

---
+++ src/main/java/com/example/shoaping_cart/ShoapingCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoaping_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoapingCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoapingCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shoaping_cart/ShoapingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoaping_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoapingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
